b2524d81ee2c13bb1355ab688ddf7dafe1fbd44b
Tanner Erekson <terekson05@gmail.com>
2023-12-03 21:30:18 -0700

    port syntax for heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Load the trail data from the JSON file
 const trailData = require('./trailData.json');
